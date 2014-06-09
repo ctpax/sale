ba76808ec25b7d3257a9a37ddf84c8556c76578d
Eric Wu <ericwu@Erics-MacBook-Pro.local>
2014-06-08 22:38:09 -0700

    rspec complete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
 	respond_to :json, :html
 	def index
+    #request shopsense api through model method, store response in @import variable 
 		@imports = Product.party(params[:limit], params[:category], params[:search])
 		respond_with @imports
 	end
@@ -15,22 +16,8 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to "#"}
+      format.html { redirect_to users_path }
       format.json { render json: { head: :ok } }
     end
-  end 
-
-   # <%= form_for :product, url: products_path do |f| %>
-   #                          <%= collection_select :page, :list_id, current_user.lists, :id, :name %>
-   #                          <%= f.hidden_field :name, :value => p["name"] %>
-   #                          <%= f.hidden_field :price, :value => p['priceLabel'] %>
-   #                          <%= f.hidden_field :sale_price, :value => p['salePriceLabel'] %>
-   #                          <%= f.hidden_field :brand, :value => p['brand']['name']  %>
-   #                          <%= f.hidden_field :url, :value => p['clickUrl'] %>
-   #                          <%= f.hidden_field :image_url, :value => p['image']['sizes']['IPhone']['url'] %>
-                        
-   #                          <%= f.submit 'add products' %>
-   #                        <% end %>
-
-  
+  end   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :set_user, only: [  :edit, :delete]
+
   respond_to :json, :html
 
  def show
@@ -7,15 +7,17 @@ class UsersController < ApplicationController
  end
 
   def edit
+    current_user
   end
 
   def new
   	@user = User.new
   end
 
-  def index
-      
+  def index 
+      #request shopsense api through model method, store response in @import variable 
       @imports = Product.party(params[:limit], params[:category], params[:search])
+      #build jason base on the user who logged in
       respond_with current_user
   end
 
@@ -29,6 +31,7 @@ class UsersController < ApplicationController
       else
         respond_to do |format|
           format.html { render 'new' }
+          #specify http rsponse code when create action fails
           format.json { render json: @user.errors, status: :unprocessable_entity}
         end
       end
@@ -42,7 +45,9 @@ class UsersController < ApplicationController
         end
       else
         respond_to do |format|
-          format.html { render edit_user_path }
+          format.html { render 'edit' }
+          #specify http rsponse code when update action fails
+          #json redner errors message
           format.json { render json: @user.errors, status: :unprocessable_entity}
         end
     end
@@ -51,17 +56,17 @@ class UsersController < ApplicationController
   def destroy
     current_user.destroy
     respond_to do |format|
-      format.html { redirect_to "#"}
+      format.html { redirect_to users_path }
       format.json { render json: { head: :ok } }
     end
   end 
 
   protected
-
+  #find the user in the data if id is in the parameters
   def set_user
     @user = User.find(params[:id])
   end
-
+  #specify what info is safe to pass into the database
   def user_params
     params.require(:user).permit(:username, :name, :email, :password, :password_confirmation)
   end
